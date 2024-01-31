6fc45cb2531ae28f71a7161ea9c794095645fa52
Alina <a.08pon@gmail.com>
2024-01-31 23:19:06 +0000

    re done task with ()

--- 3. week/saturday 27.01/hw/main.py
+++ 3. week/saturday 27.01/hw/main.py
@@ -1,17 +1,23 @@
-check_is_balanced = '()((())('
-
+check_is_balanced = '()(())'
 
 def is_balanced(string: str) -> bool: 
-    if string[0] == ')' or len(string) % 2 != 0: 
+    if string[0] == ')' or string[-1] == '(' or len(string) % 2 != 0: 
         return False
-    open = 0
-    close = 0
-    for i in string:
-        if i == '(':
-            open += 1
-        else: 
-            close += 1
-    return open == close
+    new_list = list(string)
+
+    for x in range(len(new_list)):
+        if new_list[x] == '(':
+            new_list[x] = '*'
+            next = new_list[x:].index(')')
+            if next != (-1):
+                new_list[next+x] = '*'
+    res = ''
+    try:
+        res = new_list.index(')') or new_list.index('(')
+    except ValueError: 
+        res = -1
+    return res <= -1
+
 
 
 result = is_balanced(check_is_balanced)
