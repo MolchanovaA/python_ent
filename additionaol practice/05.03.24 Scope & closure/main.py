405456f4655af79512d813dae58f6071fe413f09
Alina <a.08pon@gmail.com>
2024-03-05 11:08:45 +0000

    task added

--- additionaol practice/05.03.24 Scope & closure/main.py
+++ additionaol practice/05.03.24 Scope & closure/main.py
@@ -1,6 +1,7 @@
 
 
 # print(s, 'BEFORE)
+      
 # s = 'some'
 
 
@@ -13,122 +14,141 @@
 
 #============
 
-variable = 'a'
+# variable = 'a'
 
-def variable():
-    print('hello from var')
+# def variable():
+#     print('hello from var')
+
+# variable = 'l'
 
 # print(variable, 'what is VARIABLE')
 
 #============
 
-x = 10  
+# x = 10  
 
-def my_func():
-    x = 5  
-    # print("inside ", x)
+# def my_func():
+#     global x
+#     x = 5  
+#     print("inside ", x)
+#     # return x
 
-my_func()
+# my_func()
 # print("outside ", x)
 
 
 #==========
 
-y = 5
+# y = 5
 # print('outside 1', y) 
 
-def foo():
-    global y
-    y = 10
-    # print('inside ', y)
+# def foo():
+#     global y
+#     y = 10
+#     print('inside ', y)
 
-foo() 
+# foo() 
 # print('outside 2', y) 
 
 #============
 
-def outer():
-    x_1 = 1
-    def inner():
-        nonlocal x_1
-        x_1 = 2
-    # print("outer:", x_1)
-    inner()
-    # print("outer:", x_1)
+# def outer():
+#     x_1 = 1
+#     def inner():
+#         nonlocal x_1
+#         x_1 = 2
 
-outer()
+#     print("outer: BEFORE", x_1)
+#     inner()
+#     print("outer: AFTER", x_1)
+
+# outer()
 
 #===========
 
-foo = 1
+# foo = ['a', 'b']
 
-def bar():
-    if foo:
-        foo = 100
-    # print(foo, 'FOO')
+# def bar():
+#     global foo
+#     some = foo
+#     print(some, 'SOME')
+#     # some = '999'
+#     some.append('hhh')
+#     some += ['K']
+#     if some:
+#         print(some, foo, 'SOME + FOO')
+#         foo = 100
+#         # print(foo, 'FOO INSIDE')
 
-bar()
+# bar()
+# print(foo, 'AFTER')
 
 #============
 
-foo_1 = 1
+# foo_1 = 1
 
-def func_b():
-    global foo_1
-    foo_1 = 10
-    return 
-    def foo_1():
-        pass
+# def func_b():
+#     # global foo_1
+#     foo_1 = 10
+#     foo_1 = 888
+#     # return 
+#     def foo_1():
+#         pass
+#     print(type(foo_1))
 
-func_b()
-print(foo_1)
+# func_b()
+# print(foo_1)
 
 #============
 
 def test():
     t = 'TTT'
 
-test()
+# test()
 # print(t)
 
 
 #============
 
 
-list_x = []
+# list_x = []
 
-def test_list(l_x):
-    # print(l_x, 'inside before')
-    l_x += 'c'
-    # print(l_x, 'INSIDE after')
-    return l_x
+# def test_list(l_x):
+#     print(l_x, 'inside before')
+#     l_x += 'c'
+#     # l_x.append('J')
+#     print(l_x, 'INSIDE after')
+#     # return l_x
 
-test_list(list_x)
+# test_list(list_x)
 # print(list_x, 'AFTER OUTSIDE')
     
 #============
 
-list_y = []
+# list_y = []
+# print(id(list_y), 'BEFORE OUTSIDE')
 
-def test_list_2(l_y):
-    # print(l_y, 'INSIDE BEFORE')
-    l_y = ['A']
-    # print(l_y, 'INSIDE after')
-    return l_y
+# def test_list_2(l_y):
+#     print(id(l_y), 'INSIDE BEFORE')
+#     l_y = ['A']
+#     print(id(l_y), 'INSIDE')
+#     # print(l_y, 'INSIDE after')
+#     # return l_y
 
-test_list_2(list_y)
+# test_list_2(list_y)
+# print(id(list_y), 'BEFORE OUTSIDE')
 # print(list_y, 'OUTSIDE AFTER')
 
 #============
-b_x = '10000'
-# print(b_x, 'OUTSIDE before')
+# b_x = 10000
+# print(id(b_x), 'OUTSIDE before')
 
-if True: 
-    # print(b_x, 'INSIDE before')
-    b_x = '1'
-    # print(b_x, 'INSIDE AFTER')
+# if True: 
+#     # print(b_x, 'INSIDE before')
+#     b_x += 1
+#     print(id(b_x), 'INSIDE AFTER')
 
-# print(b_x, 'outside after')
+# print(id(b_x), 'outside after')
 
 #-=======
 
@@ -147,3 +167,28 @@ c = counter()
 
 #===========
 
+
+def outside():
+    x = 0
+    def wrapper():
+        nonlocal x 
+        x += 1
+        return x
+    
+    return wrapper
+
+t = outside()
+o = t()
+two = t()
+three = t()
+four = t()
+five = t()
+
+print(o, two, three, four, five,  'VAR T')
+
+p = outside()
+op = p()
+op_two = p()
+op_three = p()
+
+print(op,op_two,op_three, 'OP'  )
